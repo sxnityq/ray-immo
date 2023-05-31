0739a7515964d384c5256db904dbed5001c74649
Leonid Tsarov <tsarov13leo@gmail.com>
2023-05-31 07:23:34 +0200

    update sh files

--- bash_commands/cuDNN_docker_Ubuntu_20.04.sh
+++ bash_commands/cuDNN_docker_Ubuntu_20.04.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-
-mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 apt-get update
+apt-get install -y wget gnupg2 software-properties-common
 
-apt-get install libcudnn8=8.9.1.23-1+12.1
-apt-get install libcudnn8-dev=8.9.1.23-1+12.1
-apt-get install libcudnn8-samples=8.9.1.23-1+12.1
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin \
+    && mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub \
+    && apt-key add 7fa2af80.pub
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64 /" >> /etc/apt/sources.list.d/cuda.list
 
-#cp -r /usr/src/cudnn_samples_v8/ $HOME
-#cd  $HOME/cudnn_samples_v8/mnistCUDNN
-#make clean && make
-#./mnistCUDNN
+apt-get update
+apt-get install -y --no-install-recommends \
+    libcudnn8=8.9.1.23-1+cuda12.1 \
+    libcudnn8-dev=8.9.1.23-1+cuda12.1 \
+    libcudnn8-samples=8.9.1.23-1+cuda12.1
+
+cp -r /usr/src/cudnn_samples_v8/ $HOME
